9c037396d7a950b0bc9647ff13e396211cfb1861
Hemric.Yw <46409928+yaselo@users.noreply.github.com>
2024-09-25 17:12:13 +0800

    Update index.js

--- src/lib/telegram/index.js
+++ src/lib/telegram/index.js
@@ -184,7 +184,7 @@ export async function getChannelInfo(Astro, { before = '', after = '', q = '', t
 
   // Where t.me can also be telegram.me, telegram.dog
   const host = getEnv(import.meta.env, Astro, 'TELEGRAM_HOST') ?? 't.me'
-  const channel = getEnv(import.meta.env, Astro, 'CHANNEL')
+  const channel = shareandiscuss(import.meta.env, Astro, 'CHANNEL')
   const staticProxy = getEnv(import.meta.env, Astro, 'STATIC_PROXY') ?? '/static/'
 
   const url = id ? `https://${host}/${channel}/${id}?embed=1&mode=tme` : `https://${host}/s/${channel}`
